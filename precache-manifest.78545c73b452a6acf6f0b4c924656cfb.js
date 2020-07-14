bb9441c59d6a5885b7e52a7e469ea3ae0d94d377
Parag Rajhans <prajhan1@binghamton.edu>
2020-07-13 18:41:52 -0700

    Updates

---
+++ precache-manifest.78545c73b452a6acf6f0b4c924656cfb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e3688c253b74099ae5c98ded246252a",
+    "url": "/SP/index.html"
+  },
+  {
+    "revision": "af4ce569ad3e26f24fe5",
+    "url": "/SP/static/css/2.ed590494.chunk.css"
+  },
+  {
+    "revision": "e5726ea22eb2c092e4a3",
+    "url": "/SP/static/css/main.21bb3059.chunk.css"
+  },
+  {
+    "revision": "af4ce569ad3e26f24fe5",
+    "url": "/SP/static/js/2.a9100f41.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/SP/static/js/2.a9100f41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5726ea22eb2c092e4a3",
+    "url": "/SP/static/js/main.ec49bdb1.chunk.js"
+  },
+  {
+    "revision": "c35c3b0529d712e796e2",
+    "url": "/SP/static/js/runtime-main.22bc3834.js"
+  }
+]);
